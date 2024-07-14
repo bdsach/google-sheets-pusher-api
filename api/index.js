9fed64c123c41a275d787ad9b1a1149e57595e2e
Bandit Silachai <mbandit001@yahoo.co.th>
2024-07-14 13:47:44 +0700

    🔨 update(app): dynamic post data to google sheets

--- api/index.js
+++ api/index.js
@@ -1,35 +1,36 @@
 require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
+const bodyParser = require("body-parser");
 const app = express();
+
 app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /* Routes */
-app.get("/api/hello", (req, res) => {
-  res.json({ message: process.env.GOOGLE_SHEETS_ENDPOINT });
-});
-
-app.post("/api/add", async (req, res) => {
+app.post("/api/new", async (req, res) => {
+  const { user, email, text } = req.body;
   const dataToSend = [
     {
-      user: "express axios",
-      email: "alice@example.com",
-      text: "Hello, how are you today?",
+      user,
+      email,
+      text,
     },
   ];
 
-  try {
-    const response = await axios.post("https://script.google.com/macros/s/AKfycbwZN3_NvKZ09anruAExwOSS0j4J3JGTxpr1JaqOv_xl4RxrzLR_Moh521q0AFUcveri-g/exec", dataToSend);
-    res.json({
-      message: "Post is work!",
-      googleScriptResponse: response.data
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: "Error sending data to Google Script",
-      error: error.message
-    });
-  }
+    try {
+      const response = await axios.post(process.env.GOOGLE_SHEETS_ENDPOINT, dataToSend);
+      res.json({
+        message: "Post is work!",
+        googleScriptResponse: response.data
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: "Error sending data to Google Script",
+        error: error.message
+      });
+    }
 });
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
